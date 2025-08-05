949208fa03d2b692c0a7d0fb42b658178edd5660
Ayush-an <agrawalayushgopal@gmail.com>
2025-08-05 19:16:25 +0530

    Fix casing issue

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const db = require('./config/db')
 const UserModel = require('./models/user.model');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/",(req,res)=>{
     res.send("Welcome to Exam Buddy API");
